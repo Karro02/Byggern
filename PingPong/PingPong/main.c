77b07994edb264a679270560c2b4d8258f0b4fb2
Karoline Aass Olsen <karolaol@ntnu.no>
2024-09-11 18:01:12 +0200

    Mangler bare sjekke CS logic fra dag 2

--- PingPong/PingPong/main.c
+++ PingPong/PingPong/main.c
@@ -6,9 +6,49 @@
  */ 
 
 #include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define FOSC 4915200// Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
+
+#define BASE_ADDRESS 0x1000
+#define SRAM_OFFSET 0x0800
+
+void SRAM_test(void)
+{
+	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
+	uint16_t ext_ram_size = 0x800;
+	uint16_t write_errors = 0;
+	uint16_t retrieval_errors = 0;
+	printf("Starting SRAM test...\n");
+	// rand() stores some internal state, so calling this function in a loop will
+	// yield different seeds each time (unless srand() is called before this function)
+	uint16_t seed = rand();
+	// Write phase: Immediately check that the correct value was stored
+	srand(seed);
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		ext_ram[i] = some_value;
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			write_errors++;
+		}
+	}
+	// Retrieval phase: Check that no values were changed during or after the write phase
+	srand(seed);
+	// reset the PRNG to the state it had before the write phase
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			retrieval_errors++;
+		}
+	}
+	printf("SRAM test completed with \n%4d errors in write phase and \n%4d errors in retrieval phase\n\n", write_errors, retrieval_errors);
+}
 
 
 
@@ -53,19 +93,76 @@ int uart_getchar(FILE *stream) {
 	return USART_Receive();
 }
 
+void printf_init() {
+	fdevopen(uart_putchar, uart_getchar); //kan trenge referanse til funksjonen
+}
+void xmem_init() {
+	MCUCR |= 1 << SRE; // enable external memory interface
+	SFIOR &= ~(1 << XMM1 | 1 << XMM0); //setting XMM1 and XMM0 to 0 Worked with XMM2 instead of XMM0
+	SFIOR |= 1 << XMM2; // masking out the bits that JTAG uses Set other ones to 0
+	//EMCUCR = 0 << SRW10 | 0 << SRW11;
+	
+}
+
+void xmem_write ( uint8_t data , uint16_t addr ) {
+	volatile char * ext_mem = ( char *) BASE_ADDRESS ;
+	ext_mem [ addr ]= data ;
+}
+
+uint8_t xmem_read ( uint16_t addr ) {
+	volatile char * ext_mem = ( char *) BASE_ADDRESS ;
+	uint8_t ret_val = ext_mem [ addr ];
+	return ret_val ;
+}
+
 int main(void)
 {	
 	USART_Init(MYUBRR);
-
-	fdevopen(uart_putchar, uart_getchar); //kan trenge referanse til funksjonen
+	printf_init();
 
 	printf("UART initialized!\n");
-	while(1) {
+	
+	xmem_init();
+	SRAM_test();
+	
+	//printf("%4d", xmem_read(0x0832));
+	//xmem_write(0x11, 0x1);
+	//printf("%4d", xmem_read(10));
+	//xmem_write(0, 0);
+	
+	//DDRA = 1 << PA0;
+	//DDRE = 1 << PE1;
+	while (1) {
 		char r = USART_Receive();
-		if (r) {
-			USART_Transmit(r);
+		if (r == 's') {
+			xmem_write(1, SRAM_OFFSET - 1);
+			//printf("%4d", xmem_read(0x1));
+		}
+		if (r == 'd') {
+			xmem_write(1, SRAM_OFFSET + 1);
+		}
+		
+	}
+	while(0) {
+		char r = USART_Receive();
+		if (r == 'l') {
+			USART_Transmit('l');
+			PORTE = 1 << PE1;
+		} 
+		if (r == 's') {
+			USART_Transmit('s');
+			PORTE = 0 << PE1;
+		}
+		if (r == 'd') {
+			PORTA = 1;
+			USART_Transmit('d');
+		}
+		if (r == 'k') {
+			PORTA = 0;
+			USART_Transmit('k');
 		}
 	}
+
 	
 	
 	
@@ -89,5 +186,5 @@ int main(void)
 	
 	
 	return 0;
-}
+		}
 
